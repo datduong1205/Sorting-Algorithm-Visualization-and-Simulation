4a0e39562efefc434cc9092c725e861ef0046aad
Dat Duong <soundgreat12@gmail.com>
2024-03-11 13:05:57 -0400

    initial commit

---
+++ assignment_2.py
@@ -0,0 +1,16 @@
+# Name: Le Minh Dat Duong
+# ID: 100886108
+# Assignment 2 - Data Structure and Algorithms
+
+import time
+import sys
+
+product_id = [11, 1, 30, 2, 51, 6, 29, 7, 67, 15, 118, 4, 89, 23]
+
+
+
+
+
+
+
+
